d6d3558ef50e217400b8fe3447a6d8345e8ac8f1
Jay <wjdwjd1501@gmail.com>
2025-07-13 22:08:43 +0200

    Initial Commit

---
+++ SwiftHunt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftHunt
+//
+//  Created by JungWonJung on 13/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftHunt/SwiftHuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftHuntApp.swift
+//  SwiftHunt
+//
+//  Created by JungWonJung on 13/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftHuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
